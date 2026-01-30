5d45114c10f327cc5841086f4d9187a8bf04f649
disotocastro <juandisoca@gmail.com>
2026-01-30 16:33:54 -0600

    Change tab-bar icons

--- app/src/views/TabsPage.vue
+++ app/src/views/TabsPage.vue
@@ -2,26 +2,28 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
+
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon :icon="homeOutline" />
           <ion-label>Inicio</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="accessibilityOutline" />
+          <ion-icon :icon="bus" />
           <ion-label>Viajes</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-icon :icon="mailOutline" />
           <ion-label>Mensajes</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab4" href="/tabs/tab4">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-icon :icon="personOutline" />
           <ion-label>Perfil</ion-label>
         </ion-tab-button>
+
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -37,5 +39,5 @@ import {
   IonPage,
   IonRouterOutlet,
 } from "@ionic/vue";
-import { accessibilityOutline, square, triangle } from "ionicons/icons";
+import { homeOutline, bus, mailOutline, personOutline } from "ionicons/icons";
 </script>

f8817907d771d94e84316e9b96404dd77d9327ac
disotocastro <juandisoca@gmail.com>
2026-02-03 22:29:44 -0600

    add profile and settings view

--- app/src/views/Tab4Page.vue
+++ app/src/views/Tab4Page.vue
@@ -5,27 +5,47 @@
         <ion-title>Perfil</ion-title>
         <ThemeToggle />
       </ion-toolbar>
+
+      <ion-toolbar>
+        <ion-segment v-model="segment">
+          <ion-segment-button value="profile">
+            <ion-label>Profile</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="settings">
+            <ion-label>Settings</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </ion-toolbar>
     </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Perfil</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Perfil page" />
+
+      <ExploreContainer v-if="segment === 'profile'" name="Profile page" />
+      <ExploreContainer v-else name="Settings page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { 
-  IonPage, 
-  IonHeader, 
-  IonToolbar, 
-  IonTitle, 
+import { ref } from 'vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
   IonContent,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
 } from '@ionic/vue';
+
 import ExploreContainer from '@/components/ExploreContainer.vue';
 import ThemeToggle from '@/components/ThemeToggle.vue';
+
+const segment = ref<'profile' | 'settings'>('profile');
 </script>
